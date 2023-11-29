ee8b3d792394524ff40085b419d8a5576f411289
yhsdaniel <daniel.kristi09@gmail.com>
2023-11-29 09:40:32 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
-const assetPrefix = isProd ? 'template-login-nextjs' : ''
-const nextConfig = {
-    compiler: {
-        removeConsole: {
-            exclude: ['error']
-        }
-    },
-    exportPathMap: () => ({
-        '/': { page: '/' },
-    }),
-    assetPrefix: assetPrefix,
-}
+const nextConfig = {}
 
 module.exports = nextConfig
